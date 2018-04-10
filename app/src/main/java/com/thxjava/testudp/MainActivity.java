969b3843c2b682fde1c2033f96c9fbe05de1f8ed
whatisjava <pursuemoon@qq.com>
2018-04-10 14:22:25 +0800

    udp test

--- app/src/main/java/com/thxjava/testudp/MainActivity.java
+++ app/src/main/java/com/thxjava/testudp/MainActivity.java
@@ -3,13 +3,16 @@ package com.thxjava.testudp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private DatagramSocket socket;
     private DatagramPacket send_packet, receive_packet;
+
+    String wifi = "hezy-zh\0qaz!@#wsx\0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
          * 向服务器端发送数据
          */
         new UDPIMTask().execute();
+        Toast.makeText(this, "开始发送数据", Toast.LENGTH_SHORT).show();
 
+//        textView.setText(toHexString(wifi));
+
+        System.out.println(Arrays.toString(toHexString(wifi)));
     }
 
     class UDPIMTask extends AsyncTask<Void, Integer, String> {
@@ -37,71 +46,99 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... voids) {
             try {
-                // 1.定义服务器的地址、端口号、数据
+                // 1.定义服务器的地址、端口号、发送数据
                 InetAddress address = InetAddress.getByName("192.168.31.1");
                 int port = 16611;
-//                byte[] send_data = "55 00 5A 12 68 65 7A 79 2D 7A 68 71 61 7A 00 21 40 23 77 73 78 00 C2".getBytes();
-                byte[] send_data = "55 00 00 00 55".getBytes();
+
+                byte[] send_data1 = { 85, 0, 90, 18, 104, 101, 122, 121, 45, 122, 104, 0, 113, 97, 122, 33, 64, 35, 119, 115, 120, 0 };
+
+                System.out.println("sum---" + Integer.parseInt("55", 16));
+
+                byte[] send_data = { 85, 0, 90, 18, 104, 101, 122, 121, 45, 122, 104, 0, 113, 97, 122, 33, 64, 35, 119, 115, 120, 0, (byte) add(send_data1) };
 
                 // 2.创建数据报，包含发送的数据信息
                 send_packet = new DatagramPacket(send_data, send_data.length, address, port);
 
                 // 3.创建DatagramSocket对象
-                socket = new DatagramSocket();
+                socket = new DatagramSocket(8888);
+                socket.setSoTimeout(3000);
 
                 // 4.向服务器端发送数据报
                 socket.send(send_packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
 
-        @Override
-        protected void onPostExecute(String s) {
+                // 1.创建数据报，用于接收服务器端响应的数据
+                byte[] receive_data = new byte[1024];
+                receive_packet = new DatagramPacket(receive_data, receive_data.length);
 
-            /*
-             * 接收服务器端响应的数据
-             */
-            new UDPIMTask1().execute();
-
-        }
-    }
-
-    class UDPIMTask1 extends AsyncTask<Void, Integer, String> {
-
-        @Override
-        protected String doInBackground(Void... voids) {
-            // 1.创建数据报，用于接收服务器端响应的数据
-            byte[] receive_data = new byte[1024];
-            receive_packet = new DatagramPacket(receive_data, receive_data.length);
-
-            try {
                 // 2.接收服务器响应的数据
+                System.out.println("等待接收数据");
                 socket.receive(receive_packet);
+                System.out.println("接收到数据了");
 
                 // 3.读取数据
                 String reply = new String(receive_data, 0, receive_packet.getLength());
                 System.out.println("我是客户端，服务器说：" + reply);
 
+                byte[] reset = {0x55, 0x00, 0x5C, 0x00, (byte) 0xB1};
+                send_packet = new DatagramPacket(reset, reset.length, address, port);
+
+                socket.send(send_packet);
+
                 return reply;
             } catch (IOException e) {
                 e.printStackTrace();
+                System.out.println("2222222222222222222");
             }
             return null;
         }
 
+        private int add(byte[] bytes){
+            int sum = 0;
+            for (int i=0;i<bytes.length;i++) {
+                sum = sum + bytes[i];
+            }
+            return sum;
+        }
+
         @Override
         protected void onPostExecute(String s) {
-            textView.setText("我是客户端，服务器说：" + s);
+            Toast.makeText(MainActivity.this, "返回结果" + s, Toast.LENGTH_SHORT).show();
+            if (TextUtils.isEmpty(s)){
+                textView.setText("三秒后重新发送");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+
+                }
+                Toast.makeText(MainActivity.this, "三秒后重新发送", Toast.LENGTH_SHORT).show();
+                new UDPIMTask().execute();
+            } else {
+                textView.setText("我是客户端，服务器说：" + s);
+            }
             // 4.关闭资源
             socket.close();
         }
     }
 
+    public byte[] toHexString(String s) {
+        byte[] result = new byte[s.length()];
+        for (int i=0;i<s.length();i++) {
+            int ch = (int)s.charAt(i);
+//            String s4 = Integer.toHexString(ch);
+            result[i] = ((byte) ch);
+
+        }
+        return result;
+    }
+
+//    public byte[] produces(String input){
+//
+//    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        if (socket != null)
+        socket.close();
     }
 }
